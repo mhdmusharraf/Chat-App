88681939a0edbc92a044cc97758a89042ac61730
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-01-15 22:22:07 +0530

    Signup backend part done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,18 @@
 import express from "express";
-import authRoutes from './routes/auth.route.js';
-import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT=process.env.PORT
+const PORT = process.env.PORT;
 
-app.use("/api/auth",authRoutes)
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log("Server is running on PORT: "+PORT);
-    connectDB();
-})
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT: " + PORT);
+  connectDB();
+});
