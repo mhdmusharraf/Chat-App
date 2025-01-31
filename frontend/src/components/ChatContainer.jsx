a84b31b9153fc308a2f65b373e02262ca8cd3e35
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-01-31 18:07:12 +0530

    Error resolved in Message Route

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./chatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
-  return (
-    <div>
-      Chat
-    </div>
-  )
-}
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
 
-export default ChatContainer
+  if (isMessagesLoading)
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>Messages...</p>
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,13 +2,12 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
   isUserLoading: false,
   isMessagesLoading: false,
-  
 
   getUsers: async () => {
     set({ isUserLoading: true });
@@ -31,6 +30,20 @@ export const useChatStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
 
