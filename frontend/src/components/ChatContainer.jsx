85ac303293f92eabb5346f9b577cd1d5a31b0ada
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-02-02 23:51:57 +0530

    ChatContainer UI part done

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -4,6 +4,7 @@ import ChatHeader from "./chatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
@@ -34,8 +35,14 @@ const ChatContainer = () => {
               </div>
               <div className="chat-header mb-1">
                 <time className="text-xs opacity-50 ml-1">
-                  {message.createdAt}
+                 {formatMessageTime(message.createdAt)} 
                 </time>
+              </div>
+              <div className="chat-bubble flex flex-col">
+                {message.image && (
+                  <img src={message.image} alt="Attachment" className="sm:max-w-[200px] rounded-md mb-2" />
+                )}
+                {message.text && <p>{message.text}</p>}
               </div>
           </div>
         ))}
